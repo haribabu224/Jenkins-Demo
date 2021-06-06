ff201d9e58869ad67e0525a9dfbfd140699b9c82
unknown <haribabu.k224@gmail.com>
2021-06-06 17:35:02 +0530

    first Commit

---
+++ src/main/java/com/jenkins/demo/JenkinsDemoApplication.java
@@ -0,0 +1,24 @@
+package com.jenkins.demo;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDemoApplication {
+
+private static final Logger log = LoggerFactory.getLogger(JenkinsDemoApplication.class);
+
+	public static void main(String[] args) {
+		log.info("===============================MAIN METHOD=========================>>>>>>>>");
+		SpringApplication.run(JenkinsDemoApplication.class, args);
+	}
+	@PostConstruct
+	public void init() {
+		log.info("===============================INIT METHOD=========================>>>>>>>>");
+	}
+
+}
