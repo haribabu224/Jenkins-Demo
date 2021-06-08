17709707cdecfec6be8688e119be8d2d52581a37
haribabu224 <57087230+haribabu224@users.noreply.github.com>
2021-06-08 12:02:46 +0530

    Update JenkinsDemoApplication.java
    added syso

--- src/main/java/com/jenkins/demo/JenkinsDemoApplication.java
+++ src/main/java/com/jenkins/demo/JenkinsDemoApplication.java
@@ -14,6 +14,17 @@ private static final Logger log = LoggerFactory.getLogger(JenkinsDemoApplication
 
 	public static void main(String[] args) {
 		log.info("===============================MAIN METHOD=========================>>>>>>>>");
+		
+		log.info("===============================MAIN METHOD=========================>>>>>>>>");
+		
+		log.info("===============================MAIN METHOD=========================>>>>>>>>");
+		
+		log.info("===============================MAIN METHOD=========================>>>>>>>>");
+		log.info("===============================MAIN METHOD=========================>>>>>>>>");
+		log.info("===============================MAIN METHOD=========================>>>>>>>>");
+		log.info("===============================MAIN METHOD=========================>>>>>>>>");
+		log.info("===============================MAIN METHOD=========================>>>>>>>>");
+		log.info("===============================MAIN METHOD=========================>>>>>>>>");
 		SpringApplication.run(JenkinsDemoApplication.class, args);
 	}
 	@PostConstruct
